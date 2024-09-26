b37f622e8ca6ad84962d49dcc2d86c7488a0ab47
Manvitha-CS067 <manvitha.23cs067@sode-edu.in>
2024-09-26 10:23:48 +0530

    TestAbstractShape.java

--- TestAbstractShape.java
+++ TestAbstractShape.java
@@ -1,30 +1,38 @@
-
 abstract class Shape1{
         abstract double calculateArea();
-        abstract double calculatePerimeter();}
+        abstract double calculatePerimeter();
+}
 class Circle1 extends Shape1{
         private double radius;
         public Circle1(double radius) {
-                this.radius=radius;}
+                this.radius=radius;
+        }
         @Override
         double calculateArea() {
-                return Math.PI*radius*radius;}
+                return Math.PI*radius*radius;
+        }
         @Override
         double calculatePerimeter() {
-                return 2*Math.PI*radius;}}
+                return 2*Math.PI*radius;
+        }
+}
 class Triangle1 extends Shape1{
         private double side1,side2,side3;
         public Triangle1(double side1,double side2, double side3) {
                 this.side1=side1;
                 this.side2=side2;
-                this.side3=side3;}
+                this.side3=side3;
+        }
         @Override
         double calculateArea() {
                 double s=(side1+side2+side3)/2;
-                return Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));}
+                return Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
+        }
         @Override
         double calculatePerimeter() {
-                return side1+side2+side3;}}
+                return side1+side2+side3;
+        }
+}
 
 public class TestAbstractShape {
         public static void main(String[] args) {
@@ -35,4 +43,6 @@ public class TestAbstractShape {
                 Triangle1 triangle1=new Triangle1(3,4,5);
                 System.out.println("Area and Perimeter of Triangle are");
                 System.out.println(triangle1.calculateArea());
-                System.out.println(triangle1.calculatePerimeter());}}
+                System.out.println(triangle1.calculatePerimeter());
+        }
+}
